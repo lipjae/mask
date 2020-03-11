49fd990d1a28c9c9792fee956c708f0501d0d6b9
JH L <jhl@JHui-MacBookPro.local>
2020-03-12 01:31:59 +0900

    공적마스크 완성 디자인수정 필요

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,9 +7,7 @@
           <q-icon name="search" @click="searchAddress()"/>
         </template>
       </q-input>
-      <q-select v-model="selectAddress" :options="addressList" label="주소를 선택해주세요." />
-
-      {{ selectAddress }}
+      <q-select v-model="selectAddress" :options="addressList" label="주소를 선택해주세요."/>
       <div id="map" style="width:600px;height:600px;"></div>
     </div>
     
@@ -32,28 +30,74 @@ export default {
       addressList: []
     }
   },
+  watch: {
+    async selectAddress () {
+      this.setMap()
+    }
+  },
   async mounted() {
     
-    let getCurrentLocation = function () {
-      return new Promise(function (resolve, reject) {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-      })
-    }
-    try {
-      let currentLatLng = await getCurrentLocation()  
+    // let getCurrentLocation = function () {
+    //   return new Promise(function (resolve, reject) {
+    //     navigator.geolocation.getCurrentPosition(resolve, reject);
+    //   })
+    // }
+  },
+  methods: {
+    async getStore (lat, lng) {
+      try {
+        const stores = await this.$axios.get('https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByGeo/json?',{
+          params: {
+            lat: lat,
+            lng: lng,
+            m: 3000
+          }
+        })  
+        return stores.data.stores
+      } catch (error) {
+        alert('약국 정보를 가져올수 없습니다.')
+      }
+    },
+    async searchAddress () {     
+      try {
+        let query = `query=${this.searchKeyword}`
+        let address = await this.$axios.get(`https://dapi.kakao.com/v2/local/search/address.json?${query}`,{
+          headers: {
+            Authorization: 'KakaoAK d62cabb2520e0a6ecb26b19405cee1da'
+          }
+        })
+        let addrList = []
+        _.each(address.data.documents, function(value, key){
+          addrList.push({
+            label: value.address_name,
+            value: value
+          })
+        })
+        this.addressList = addrList
+      } catch (error) {
+        throw error  
+      }
+    },
+    async setMap () {
+      let lat = this.selectAddress.value.y
+      let lng = this.selectAddress.value.x
+      console.log(lat, lng)
       var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
       var options = { //지도를 생성할 때 필요한 기본 옵션
-        center: new kakao.maps.LatLng(currentLatLng.coords.latitude, currentLatLng.coords.longitude), //지도의 중심좌표.
+        center: new kakao.maps.LatLng(lat, lng), //지도의 중심좌표.
         level: 3 //지도의 레벨(확대, 축소 정도)
       };
       var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
       
-
-      const stores = await this.getStore()
+      const stores = await this.getStore(lat, lng)
       let positions = []
       _.each(stores, (value, key) => {
         positions.push({
-          content: `<div><p>${value.name}</p></div>`,
+          content: `<div>
+              <p>${value.name}</p>
+              <p>재고상태: ${value.remain_stat}</p>
+              <p>업데이트 시간: ${value.stock_at}</p>
+            </div>`,
           latlng: new kakao.maps.LatLng(value.lat, value.lng)
         })
       })  
@@ -88,54 +132,20 @@ export default {
         kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
         kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
       }
-    } catch (error) {
-      alert('현재위치를 가져올수 없습니다.')
-      return
-    }   
     
 
-    // 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
-    function makeOverListener(map, marker, infowindow) {
-        return function() {
-            infowindow.open(map, marker);
-        };
-    }
-
-    // 인포윈도우를 닫는 클로저를 만드는 함수입니다 
-    function makeOutListener(infowindow) {
-        return function() {
-            infowindow.close();
-        };
-    }
-  },
-  methods: {
-    async getStore () {
-      try {
-        const stores = await this.$axios.get('https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/stores/json')  
-        return stores.data.storeInfos
-      } catch (error) {
-        alert('약국 정보를 가져올수 없습니다.')
+      // 인포윈도우를 표시하는 클로저를 만드는 함수입니다 
+      function makeOverListener(map, marker, infowindow) {
+          return function() {
+              infowindow.open(map, marker);
+          };
       }
-    },
-    async searchAddress () {     
-      try {
-        let query = `query=${this.searchKeyword}`
-        let address = await this.$axios.get(`https://dapi.kakao.com/v2/local/search/address.json?${query}`,{
-          headers: {
-            Authorization: 'KakaoAK d62cabb2520e0a6ecb26b19405cee1da'
-          }
-        })
-        let addrList = []
-        _.each(address.data.documents, function(value, key){
-          addrList.push({
-            label: value.address_name,
-            value: value
-          })
-        })
-        this.addressList = addrList
-        
-      } catch (error) {
-        throw error  
+
+      // 인포윈도우를 닫는 클로저를 만드는 함수입니다 
+      function makeOutListener(infowindow) {
+          return function() {
+              infowindow.close();
+          };
       }
     }
   }
